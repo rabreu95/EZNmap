6c2bb782f48a204af22ab43bed27d40fd932040b
rabreu95 <147789281+rabreu95@users.noreply.github.com>
2023-10-13 00:46:29 -0400

    formatting

--- eznmap.sh
+++ eznmap.sh
@@ -4,11 +4,16 @@
 
 
 echo "$opener1"
-echo "$opener2"
 
-read -p "Please enter the host: " host
-read -p "How fast would you like to run this scan (Please enter a single digit between 1-5): " speed
-read -p "Please enter the command: " com
-read -p "Enter a name for the output file: " output
+echo "$opener2"
+echo " "
+echo "Please enter the host" 
+read -p ">> " host
+echo "How fast would you like to run this scan (Please enter a single digit between 1-5)"
+read -p ">> " speed
+echo "Please enter the command"
+read -p ">> " com
+echo "Enter a name for the output file" 
+read -p ">>" output
 
 $com
