3ab39d286e7c463f1cb61bdec0dcd72245141ac7
rabreu95 <147789281+rabreu95@users.noreply.github.com>
2023-10-19 01:30:43 -0400

    Added -Pn to most commands

--- eznmaplib.sh
+++ eznmaplib.sh
@@ -25,29 +25,29 @@ dos() {
     nmap $host -T$speed -max-parallelism 800 -Pn --script http-slowloris --script-args http-slowloris.runforever=true 
 }
 mal() {
-    nmap -p80 -T$speed --script http-google-malware $host 
+    nmap -p80 -Pn -T$speed --script http-google-malware $host 
 }
 os() {
-    nmap -A -T$speed $host 
+    nmap -A -Pn -T$speed $host 
 }
 syn() {
     nmap -sS -p0- -Pn -A -v -T$speed $host 
 }
 
 http-discov() {
-    nmap --script http-enum -sV -v -T$speed $host 
+    nmap --script http-enum -Pn -sV -v -T$speed $host 
 }
 
 basic-xss() {
-    nmap -sV --script -T$speed -v http-unsafe-output-escaping $host 
+    nmap -sV -Pn --script -T$speed -v http-unsafe-output-escaping $host 
 }
 
 basic-sql-inject() {
-    nmap -sV -T$speed -v --script http-sql-injection $host 
+    nmap -sV -Pn -T$speed -v --script http-sql-injection $host 
 }
 
 basic-wp-bf() {
-    nmap -sV -T$speed -v --script http-wordpress-brute $host 
+    nmap -sV -Pn -T$speed -v --script http-wordpress-brute $host 
 }
 
 top-ports() {
@@ -55,7 +55,7 @@ top-ports() {
 }
 
 ftp-bf(){
-    nmap --script ftp-brute -v -p 21 -T$speed $host 
+    nmap -Pn --script ftp-brute -v -p 21 -T$speed $host 
 }
 
 
